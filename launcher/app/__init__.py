c986ffca1cabb1653e7dc8a53da3c38cdfe09116
bq22005 <bq22005@shibaura-it.ac.jp>
2025-05-15 00:51:31 +0900

    Add UI to launch honeypot manually

--- launcher/app/__init__.py
+++ launcher/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-  app = Flask(__name__)
+  app = Flask(__name__, static_url_path='/static', static_folder='static')
 
   from app.routes import bp
   app.register_blueprint(bp)
--- launcher/app/routes.py
+++ launcher/app/routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, jsonify
 from app.controller import docker_manager, session_manager
+import os
+import json
 
-bp = Blueprint('main', __name__)
+bp = Blueprint('main', __name__, static_url_path='/static', static_folder='static')
+
+@bp.route('/')
+def index():
+  return render_template("index.html")
+
+@bp.route('/api/logs/cowrie', methods=['GET'])
+def get_cowrie_logs():
+  file_path = os.path.join(os.path.dirname(__file__), '../static/data/cowrie/cowrie.json')
+
+  try:
+    with open(file_path, 'r', encoding='utf-8') as f:
+      logs = [json.loads(line) for line in f if line.strip()]
+    return jsonify(logs)
+  except FileNotFoundError:
+    return jsonify({'error': 'cowrie.json not found'}), 404
 
 @bp.route('/trigger/http', methods=['POST'])
 def trigger_http():
@@ -15,10 +32,10 @@ def trigger_http():
 
 @bp.route('/trigger/ssh', methods=['POST'])
 def trigger_ssh():
-  session_manager.update_session("ssh-honeypot")
+  session_manager.update_session("cowrie")
 
-  with session_manager._services["ssh-honeypot"].pause_lock:
-    if not docker_manager.is_service_running("ssh-honeypot"):
-      docker_manager.unpause_service("ssh-honeypot")
+  with session_manager._services["cowrie"].pause_lock:
+    if not docker_manager.is_service_running("cowrie"):
+      docker_manager.unpause_service("cowrie")
   
   return "SSH Honeypot Triggered", 200
