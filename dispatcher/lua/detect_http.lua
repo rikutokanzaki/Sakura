306c06533f674b83fa2fc504d11eeb6524afd479
bq22005 <bq22005@shibaura-it.ac.jp>
2025-05-17 03:23:01 +0900

    Update honeypot endpoint

--- dispatcher/lua/detect_http.lua
+++ dispatcher/lua/detect_http.lua
@@ -12,7 +12,7 @@ local http_patterns = {
 for _, p in ipairs(http_patterns) do
   if uri:find(p, 1, true) or ua:find(p, 1, true) then
     local launcher_port = "5000"
-    local launcher_address = "http://launcher:" .. launcher_port .. "/trigger/http"
+    local launcher_address = "http://launcher:" .. launcher_port .. "/trigger/snare"
     local res, err = httpc:request_uri(launcher_address, {
       method = "POST",
     })
