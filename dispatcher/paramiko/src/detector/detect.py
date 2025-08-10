6d057061e917b33cdae039f1baa70516db3f4282
rikutokanzaki <rikkuto@icloud.com>
2025-08-10 22:08:47 +0900

    Update black line at the end

--- dispatcher/paramiko/src/detector/detect.py
+++ dispatcher/paramiko/src/detector/detect.py
@@ -1,7 +1,7 @@
 class AttackDetector:
   def __init__(self):
     self.failed_attempts = 0
-  
+
   def check(self, username, password, command):
     if username == "root" and password == "1234":
       return True
