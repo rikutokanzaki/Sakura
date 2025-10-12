f9e2b4238a34fa1eed197deb246295171d159c3c
rikutokanzaki <rikkuto@icloud.com>
2025-10-13 02:11:13 +0900

    Fix memory leak

--- dispatcher/paramiko/src/connector/connect_server.py
+++ dispatcher/paramiko/src/connector/connect_server.py
@@ -9,6 +9,8 @@ class SSHConnector:
     self.port = port
 
   def record_login(self, username: str, password: str):
+    client = None
+
     try:
       client = paramiko.SSHClient()
       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -26,6 +28,9 @@ class SSHConnector:
           pass
 
   def replay_history(self, chan, username: str, password: str, history: list[str]):
+    client = None
+    shell = None
+
     try:
       client = paramiko.SSHClient()
       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -78,6 +83,9 @@ class SSHConnector:
           pass
 
   def replay_cwd_only(self, username: str, password: str, history: list[str]) -> str:
+    client = None
+    shell = None
+
     try:
       client = paramiko.SSHClient()
       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -113,6 +121,9 @@ class SSHConnector:
           pass
 
   def execute_command(self, command: str, username: str, password: str, dir_cmd=None):
+    client = None
+    shell = None
+
     try:
       client = paramiko.SSHClient()
       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -151,6 +162,9 @@ class SSHConnector:
           pass
 
   def execute_with_tab(self, cwd, command: str, username: str, password: str):
+    client = None
+    shell = None
+
     try:
       client = paramiko.SSHClient()
       client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
